59b5c51470d3f06c36332c9aa7ce9e136ad7527b
Andrey <andrey-ssk98@yandex.ru>
2026-02-11 21:32:19 +0300

    AddNewFunc

--- CustomHashMap.java
+++ CustomHashMap.java
@@ -1,17 +1,45 @@
 //Реализовать кастомную реализацию HashMap с методами put, get, remove.
 
 public class CustomHashMap<K, V> {
-    private static final int CAPACITY = 100;
-    private Object[][] table = new Object[CAPACITY][2];
+    private static final double LOAD_FACTOR = 0.75;
+    private int capacity = 16;
+    private int size = 0;
+    private Object[][] table;
+
+    public CustomHashMap() {
+        table = new Object[capacity][2];
+    }
+
+    private void resize() {
+     capacity *= 2;
+     Object[][] oldTable = table;
+     table = new Object[capacity][2];
+     size = 0;
+
+     for (int i = 0; i < oldTable.length; i++) {
+         if (oldTable[i][0] != null) {
+             put((K) oldTable[i][0], (V) oldTable[i][1]);
+         }
+     }
+    }
 
     public void put(K key, V value) {
-        int index = Math.abs(key.hashCode()) % CAPACITY;
+        if ((double) size / capacity >= LOAD_FACTOR) {
+            resize();
+        }
+        int index = Math.abs(key.hashCode()) % capacity;
+
+        if (table[index][0] != null && table[index][0].equals(key)){
+            table[index][1] = value;
+            return;
+        }
         table[index][0] = key;
         table[index][1] = value;
+        size++;
     }
 
     public V get(K key) {
-        int index = Math.abs(key.hashCode()) % CAPACITY;
+        int index = Math.abs(key.hashCode()) % capacity;
         if (table[index][0] != null && table[index][0].equals(key)) {
             return (V) table[index][1];
         }
@@ -19,13 +47,18 @@ public class CustomHashMap<K, V> {
     }
 
     public V remove(K key) {
-        int index = Math.abs(key.hashCode()) % CAPACITY;
+        int index = Math.abs(key.hashCode()) % capacity;
         if (table[index][0] != null && table[index][0].equals(key)) {
             V value = (V) table[index][1];
             table[index][0] = null;
             table[index][1] = null;
+            size--;
             return value;
         }
         return null;
+    }
+
+    public int size() {
+        return size;
     }
 }
